9281bd6950394c8d93ef81c1c3059d8f189eaa32
Ahmed Orabi <aorabi@outlook.com>
2025-09-26 21:18:47 +0300

    fix: change database type from postgres to mysql and update default port

--- data-source.ts
+++ data-source.ts
@@ -4,9 +4,9 @@ import { config } from 'dotenv';
 config();
 
 export default new DataSource({
-    type: 'postgres',
+    type: 'mysql',
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || '5432'),
+    port: parseInt(process.env.DB_PORT || '3306'),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
